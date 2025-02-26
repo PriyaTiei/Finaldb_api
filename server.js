6fd74e891dd17ac2c6be54e80c7b32ce0feb5d31
Sanjana <145703584+Sanjana1407a@users.noreply.github.com>
2025-02-26 20:58:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -483,15 +483,6 @@ app.get('/api/torque-data', async (req, res) => {
       });
     }
     
-    // Get mapped folders for the station
-    const stationFolders = getStationFolders(station);
-    
-    // Return error if station is not mapped
-    if (stationFolders.length === 0) {
-      return res.status(404).json({
-        error: `Station ${station} is not configured in the mapping.`
-      });
-    }
     
     let combinedData = [];
     let foundData = false;
